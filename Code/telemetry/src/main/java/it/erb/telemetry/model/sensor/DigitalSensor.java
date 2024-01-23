0eabe97ba0b64e2ace156b792656174e5c44e9d7
SM-15 <simone.manenti@live.it>
2024-01-23 15:48:33 +0100

    Fix funzione retrieveRecord da database

--- Code/telemetry/src/main/java/it/erb/telemetry/model/sensor/DigitalSensor.java
+++ Code/telemetry/src/main/java/it/erb/telemetry/model/sensor/DigitalSensor.java
@@ -1,13 +1,20 @@
 package it.erb.telemetry.model.sensor;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+
 public class DigitalSensor extends Sensor
 {
 	private boolean value;
 	
 	public boolean getValue() { return value; }
+	
+	public BooleanProperty  getProperty() { return new SimpleBooleanProperty(value); }
 
 	public void setValue(boolean value) { this.value = value; }
 	
 	public void setValue(String value) { this.value = value.equals("1"); }
-		
+	
+	
 }
