1bef779461c9b8ce0370c864e05cedbad470b4a1
nonfederico <105061749+nonfederico@users.noreply.github.com>
2024-01-24 22:29:27 +0100

    Aggiunto TabPane
    per organizzare le interfacce

---
+++ Code/telemetry/src/main/java/it/erb/telemetry/view/LinearCharts.java
@@ -0,0 +1,19 @@
+package it.erb.telemetry.view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class LinearCharts  {
+/*
+NumberAxis xAxis = new NumberAxis(0,100,10);
+
+public void returnX() {
+	xAxis.
+}
+*/
+}
+
