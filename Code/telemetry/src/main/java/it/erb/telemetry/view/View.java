b66f59c741239ae04873db54fa6274faddb0eae5
nonfederico <105061749+nonfederico@users.noreply.github.com>
2024-01-28 16:38:57 +0100

    grafici
    eccezioni inserendo grafici nel tabpane

--- Code/telemetry/src/main/java/it/erb/telemetry/view/View.java
+++ Code/telemetry/src/main/java/it/erb/telemetry/view/View.java
@@ -16,6 +16,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
@@ -299,6 +302,27 @@ public class View
 		tableHistoryDataPane.getChildren().add(tableView);
 		tableHistoryDataPane.setPadding(new Insets(5));
 		tableView.prefHeightProperty().bind(tableHistoryDataPane.heightProperty());
+		
+		// Grafici
+		NumberAxis xAxis = new NumberAxis();
+		xAxis.setLabel("x");
+		NumberAxis yAxis = new NumberAxis();
+		yAxis.setLabel("y");
+		LineChart lineChart = new LineChart(xAxis, yAxis);
+		
+		XYChart.Series dataSeries1 = new XYChart.Series();
+		dataSeries1.setName("2014");
+
+		dataSeries1.getData().add(new XYChart.Data( 1, 567));
+		dataSeries1.getData().add(new XYChart.Data( 5, 612));
+		dataSeries1.getData().add(new XYChart.Data(10, 800));
+		dataSeries1.getData().add(new XYChart.Data(20, 780));
+		dataSeries1.getData().add(new XYChart.Data(40, 810));
+		dataSeries1.getData().add(new XYChart.Data(80, 850));
+
+		lineChart.getData().add(dataSeries1);
+		
+		tilePane.getChildren().add(lineChart);
 		
 		// CENTER PANE
 		TabPane tabPane = new TabPane();
